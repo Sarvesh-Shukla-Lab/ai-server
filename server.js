cefd7fac073088b99350e8cc0ca811c942a6a26f
Sarvesh-Shukla-Lab <sarveshshukla485@gmail.com>
2026-02-18 09:47:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+app.post("/chat", async (req, res) => {
+    const userMessage = req.body.message;
+
+    res.json({
+        reply: "Tumne bola: " + userMessage
+    });
+});
+
 app.get("/", (req, res) => {
-  res.send("AI server is running 🚀");
+    res.send("AI server is running 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server started on port " + PORT);
+    console.log("Server started on port " + PORT);
 });
